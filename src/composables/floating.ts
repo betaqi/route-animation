052d108cb742a1a380c38ff2c16a97180be1701f
贝塔棋 <3188864257@qq.com>
2023-03-21 21:33:28 +0800

    feat: 组件起飞降落实现

--- src/composables/floating.ts
+++ src/composables/floating.ts
@@ -1,69 +1,69 @@
-import type { Component, StyleValue } from 'vue'
+import { Component, StyleValue, Teleport } from 'vue'
 import { defineComponent } from 'vue'
-export function createFloating(component: Component) {
-  const matedData = reactive({
-    props: {},
-    attrs: {},
-  })
-  const proxyEl = ref<Element>()
-  // eslint-disable-next-line vue/one-component-per-file
+
+export const createFloating = (component: Component) => {
+  const attrs = ref()
+  const proxyEl = ref<HTMLElement>()
+
+  let { top, left } = useElementBounding(proxyEl)
+  const island = ref(false)
   const Container = defineComponent({
     setup() {
-      const observerOptions = {
-        childList: true,
-        attributes: true,
-        subtree: true,
-      }
-
-      const rect = ref<DOMRect | undefined>()
+      const router = useRouter()
       const Style = computed((): StyleValue => {
-        if (!proxyEl.value) {
-          return {
-            display: 'none',
-            translate: '',
-          }
-        }
+        console.log(island.value)
         return {
           position: 'fixed',
-          transition: 'all .5s',
-          top: `${rect.value?.top ?? 0}px`,
-          left: `${rect.value?.left ?? 0}px`,
+          transition: 'all 1s ease-in-out',
+          top: `${top.value}px`,
+          left: `${left.value}px`,
+          display: `${island.value ? 'none' : 'block'}`,
+          boxShadow: ' 5px 5px red'
         }
       })
-
-      const observer = new MutationObserver((mutations) => {
-        const element = mutations[0].target as Element
-        rect.value = element.getBoundingClientRect()
+      const cleanRouterGuard = router.beforeEach(async () => {
+        island.value = false
+        await nextTick()
       })
 
-      function update() {
-        rect.value = proxyEl.value?.getBoundingClientRect()
-        proxyEl.value && observer.observe(proxyEl.value, observerOptions)
-      }
+      onBeforeUnmount(() => {
+        cleanRouterGuard()
+      })
+      const transitionAttrs = ref([])
+      const counter = ref(0)
 
-      window.addEventListener('resize', update)
-      watchEffect(update)
-      return () => h('div', { style: Style.value }, [
-        h(component, matedData.attrs),
-      ])
+
+      return () => h(
+        'div', {
+        style: Style.value, id: 'container',
+        onTransitionstart: (e: { propertyName: never }) => {
+          transitionAttrs.value.push(e.propertyName)
+        },
+        onTransitionend: async () => {
+          counter.value++
+          console.log(counter.value, transitionAttrs.value.length)
+          console.log('end')
+          if (counter.value === transitionAttrs.value.length) {
+            await nextTick()
+            island.value = true
+            transitionAttrs.value = []
+            counter.value = 0
+          }
+        }
+      },
+        h(Teleport,
+          { to: island.value ? '#proxy' : 'body', disabled: !island.value },
+          h(component, attrs.value)
+        )
+      )
     },
 
   })
-  // eslint-disable-next-line vue/one-component-per-file
   const Proxy = defineComponent({
     setup() {
       const attr = useAttrs()
-      matedData.attrs = attr
-      const el = ref()
-      onMounted(() => {
-        proxyEl.value = el.value
-      })
-
-      onUnmounted(() => {
-        proxyEl.value = undefined
-      })
-
-      return () => h('div', { ref: el }, [])
+      attrs.value = attr
+      return () => h('div', { ref: proxyEl, id: 'proxy' }, [])
     },
   })
 

e4c335ddb17688f088319de9d75a2a3979d03533
贝塔棋 <3188864257@qq.com>
2023-03-22 15:48:51 +0800

    fix:  图片销毁隐藏逻辑

--- src/composables/floating.ts
+++ src/composables/floating.ts
@@ -11,14 +11,13 @@ export const createFloating = (component: Component) => {
     setup() {
       const router = useRouter()
       const Style = computed((): StyleValue => {
-        console.log(island.value)
+        if (!proxyEl.value) return { display: 'none' }
         return {
           position: 'fixed',
           transition: 'all 1s ease-in-out',
           top: `${top.value}px`,
           left: `${left.value}px`,
           display: `${island.value ? 'none' : 'block'}`,
-          boxShadow: ' 5px 5px red'
         }
       })
       const cleanRouterGuard = router.beforeEach(async () => {
@@ -41,8 +40,6 @@ export const createFloating = (component: Component) => {
         },
         onTransitionend: async () => {
           counter.value++
-          console.log(counter.value, transitionAttrs.value.length)
-          console.log('end')
           if (counter.value === transitionAttrs.value.length) {
             await nextTick()
             island.value = true
@@ -63,7 +60,7 @@ export const createFloating = (component: Component) => {
     setup() {
       const attr = useAttrs()
       attrs.value = attr
-      return () => h('div', { ref: proxyEl, id: 'proxy' }, [])
+      return () => h('div', { ref: proxyEl, id: 'proxy' })
     },
   })
 
